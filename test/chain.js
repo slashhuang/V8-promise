8542e97123b9c94e5bccf1906f861c67dd22153c
slashhuang <627284482@qq.com>
2017-03-26 12:33:34 +0800

    add chain test

--- test/chain.js
+++ test/chain.js
@@ -8,17 +8,18 @@
 let SuperPromise = require('../promise');
 let assert = require('assert');
 
-describe('async Promise test should work', function() {
-  describe('async Promise test', function(done) {
+describe('chained Promise test should work', function() {
+  describe('chained Promise test', function(done) {
     it('should return 700', function() {
         let asyncTest = new SuperPromise((res,rej)=>{
 			setTimeout(res,1000,500)
 		});
 		let asyncTest1 = asyncTest.then((val)=>{
-			return val+100
+			return new SuperPromise((res,rej)=>{
+				setTimeout(res,1000,(100+val))
+			}).then(val=>val+100)
 		});
 		asyncTest1.then(val=>assert.equal(700,val)).then(done)
-
     });
   });
 });
